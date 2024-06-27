be598880e2125daad918f55f47df76e2596eaee3
Patrick <patasax22@yahoo.com>
2024-06-27 15:26:53 -0400

    info center section basic layout complete

--- src/App.java
+++ src/App.java
@@ -1,20 +1,40 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
+
+    private InfoCenter infoCenter;
+
     @Override
     public void start(Stage primaryStage)
     {
-        VBox vbox = new VBox(10);
-        Scene scene = new Scene(vbox,640,480);
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, UIConstants.APP_WIDTH, UIConstants.APP_HEIGHT);
+        initLayout(root);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Default");
+        primaryStage.setTitle("Tic-Tac-Toe");
         primaryStage.show();
+    }
+
+    private void initLayout(BorderPane root) {
+        initInfoCenter(root);
+        initTileBoard(root);
+    }
+
+    private void initInfoCenter (BorderPane root){
+        infoCenter = new InfoCenter();
+
+        root.getChildren().add(infoCenter.getStackPane());
+        
+    }
+
+    private void initTileBoard (BorderPane root){
+
     }
 
 }
